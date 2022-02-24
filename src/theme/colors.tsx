fbcee78f11c360ed023f178e2a04afe073813df5
Melker <melker.programmering@gmail.com>
2022-02-24 21:15:03 +0100

    Folder colors

---
+++ src/theme/colors.tsx
@@ -0,0 +1,16 @@
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+type FolderColor = { name: string, color: string }
+
+
+
+export const FOLDER_COLORS: FolderColor[] = [
+  { color: "rgb(123, 209, 72)", name: "Green"},
+  { color: "#1f94e0", name: "Blue" },
+  { color: "rgb(248, 58, 34)", name: "Red" },
+  { color: "rgb(255, 173, 70)", name: "Orange"},
+  { color: "rgb(250, 209, 101)", name: "Yellow" },
+  { color:  "rgb(164, 122, 226)", name: "Violet"},
+  { color:  "rgb(95, 99, 104)", name: "Grey"}
+
+]
