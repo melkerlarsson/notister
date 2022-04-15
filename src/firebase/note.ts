b83a913c1ad09cb14efe4b3e753f2dc6eae64c44
Melker Larsson <melker.programmering@gmail.com>
2022-04-15 12:26:03 +0200

    Rename and delete

--- src/firebase/note.ts
+++ src/firebase/note.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import { uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { DocumentReference, updateDoc } from "firebase/firestore";
+import { uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
 import { notesStorageRef } from "./config";
+import { ApiResponse, ErrorMessage } from "./types";
 
 const convertImageToBlob = async (url: string): Promise<Blob> => {
 	const config: AxiosRequestConfig = { url: url, method: "GET", responseType: "blob" };
@@ -14,7 +16,7 @@ type UploadImageProps = {
 	id: string;
 	onUploaded: (remoteUrl: string) => void;
 	onProgressChanged?: (progress: number) => void;
-  onError: (message: string) => void;
+	onError: (message: string) => void;
 };
 export const uploadImage = async ({ url, id, onUploaded, onProgressChanged, onError }: UploadImageProps): Promise<void> => {
 	const blob = await convertImageToBlob(url);
@@ -43,3 +45,47 @@ export const uploadImage = async ({ url, id, onUploaded, onProgressChanged, onEr
 	);
 };
 
+type DelteNoteProps = {
+	id: string;
+	parentFolderRef: DocumentReference<RootFolder | Folder>;
+	notes: Note[];
+};
+
+export const deleteNoteAndRemoveFromFolder = async ({ id, parentFolderRef, notes }: DelteNoteProps): Promise<ApiResponse<Note[]>> => {
+	// TODO: Delete learning data?
+
+	try {
+		await deleteNote(id);
+
+		const newNotes = notes.filter((note) => note.id !== id);
+		await updateDoc(parentFolderRef, { notes: newNotes });
+
+		return { data: newNotes, error: null };
+	} catch (error) {
+		return { error: { title: "Error", description: "Error deleting note. Please try again" }, data: null };
+	}
+};
+
+const deleteNote = async (id: string): Promise<void> => {
+	const ref = notesStorageRef(id);
+	await deleteObject(ref);
+};
+
+type RenameNoteProps = {
+	id: string;
+	name: string;
+	parentFolderRef: DocumentReference<RootFolder | Folder>;
+	notes: Note[];
+};
+
+export const renameNote = async ({ id, name, parentFolderRef, notes }: RenameNoteProps): Promise<ApiResponse<Note[]>> => {
+	const noteToRenameIndex = notes.findIndex((note) => note.id === id);
+	try {
+		notes[noteToRenameIndex].name = name;
+		await updateDoc(parentFolderRef, { notes: notes });
+
+		return { data: notes, error: null };
+	} catch (error) {
+		return { error: { title: "Error", description: "Error renaming note. Please try again" }, data: null };
+	}
+};
