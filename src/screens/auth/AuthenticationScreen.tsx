1439b66e02988541b267dbd6e89a24b8c0c8d8d3
Melker Larsson <melker.programmering@gmail.com>
2022-04-23 20:28:26 +0200

    Spelling error

--- src/screens/auth/AuthenticationScreen.tsx
+++ src/screens/auth/AuthenticationScreen.tsx
@@ -8,7 +8,7 @@ const AuthenticationScreen = ({ navigation }: AuthenticationScreenProps) => {
 	return (
 		<View style={styles.container}>
 			<Text style={{ fontSize: 54, fontWeight: "bold", alignSelf: "flex-start" }}>Notiser</Text>
-			<Text style={{ fontSize: 18, alignSelf: "flex-start", marginBottom: 20 }}>Start reviewing you notes today.</Text>
+			<Text style={{ fontSize: 18, alignSelf: "flex-start", marginBottom: 20 }}>Start reviewing your notes today.</Text>
 			<View style={styles.buttonContainer}>
 				<Button style={[styles.button, { marginVertical: 15 }]} title="Log In" onPress={() => navigation.push("SignIn")} />
 				<Button style={styles.button} inverted title="Sign Up" onPress={() => navigation.push("SignUp")} />
